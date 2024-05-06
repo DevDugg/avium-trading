01d15ed4e5121744362f1300eb36c25184a1f6cd
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-05-07 01:02:10 +0900

    loading eager unconditional

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,20 +1,13 @@
 import Image from "next/image";
 
 interface LogoProps {
-  eager?: boolean;
   size?: number;
 }
 
-const Logo = ({ eager, size }: LogoProps) => {
+const Logo = ({ size }: LogoProps) => {
   return (
     <div className="logo rounded-[10px] w-12 h-12 overflow-hidden" style={size ? { width: size, height: size } : {}}>
-      <Image
-        alt="Logo"
-        loading={eager ? "eager" : "lazy"}
-        src={"/images/logo.png"}
-        width={size || 48}
-        height={size || 48}
-      />
+      <Image alt="Logo" loading="eager" src={"/images/logo.png"} width={size || 48} height={size || 48} />
     </div>
   );
 };
