87936376d2fcbcdc5609fc84d06e11a422c2d31c
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-05-07 21:08:05 +0900

    testimonials section animated on scroll

--- src/components/testimonials/testimonials.tsx
+++ src/components/testimonials/testimonials.tsx
@@ -1,20 +1,26 @@
+import AnimateInView from "../animate-in-view";
 import SectionContainer from "../structure/section-container";
 import SectionTitle from "../structure/section-title";
 import TestimonialColon from "./testimonial-colon";
+import { defaultScrollVariants } from "@/variants/default-scroll.variants";
 
 const Testimonials = () => {
   return (
     <section className="testimonials" id="testimonials">
       <SectionContainer>
-        <SectionTitle>What others say about us</SectionTitle>
-        <div className="relative max-h-[1413px] h-full">
-          <div className="absolute top-0 left-0 w-full h-full bg-TESTIMONIALS pointer-events-none z-10"></div>
-          <div className="flex gap-4">
-            <TestimonialColon direction="up" />
-            <TestimonialColon direction="down" />
-            <TestimonialColon direction="up" />
+        <AnimateInView {...defaultScrollVariants}>
+          <SectionTitle>What others say about us</SectionTitle>
+        </AnimateInView>
+        <AnimateInView {...defaultScrollVariants} threshold={0.2}>
+          <div className="relative max-h-[1413px] h-full">
+            <div className="absolute top-0 left-0 w-full h-full bg-TESTIMONIALS pointer-events-none z-10"></div>
+            <div className="flex gap-4">
+              <TestimonialColon direction="up" />
+              <TestimonialColon direction="down" />
+              <TestimonialColon direction="up" />
+            </div>
           </div>
-        </div>
+        </AnimateInView>
       </SectionContainer>
     </section>
   );
