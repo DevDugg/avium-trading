f81c5a19e9c3ba214e8657200e7c59cc414d29a1
VividTruthKeeper <mr.aydogdy02@gmail.com>
2024-05-11 15:03:51 +0900

    responsive structure elements

--- src/components/structure/section-container.tsx
+++ src/components/structure/section-container.tsx
@@ -2,6 +2,6 @@ import Container from "../container";
 import { PropsWithChildren } from "react";
 
 const SectionContainer = ({ children }: PropsWithChildren) => {
-  return <Container className="flex flex-col gap-12">{children}</Container>;
+  return <Container className="flex flex-col gap-12 max-lg:gap-10">{children}</Container>;
 };
 export default SectionContainer;
--- src/components/structure/section-title.tsx
+++ src/components/structure/section-title.tsx
@@ -3,6 +3,6 @@ interface SectionTitleProps {
 }
 
 const SectionTitle = ({ children }: SectionTitleProps) => {
-  return <h2 className="text-H2 font-bold text-center">{children}</h2>;
+  return <h2 className="text-H2 font-bold text-center max-lg:text-[32px] max-md:text-2xl">{children}</h2>;
 };
 export default SectionTitle;
