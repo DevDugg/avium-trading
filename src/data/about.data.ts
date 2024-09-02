d2fbd64d857afc58ac655654bed23be042e9a93a
ThePidgeon <97064203+ThePidgeon@users.noreply.github.com>
2024-09-02 19:34:32 +0200

    Update about.data.ts

--- src/data/about.data.ts
+++ src/data/about.data.ts
@@ -1,7 +1,7 @@
 export const aboutData = {
   title: "Your path to success",
   paragraphs: [
-    `Avium was created by traders who wanted to teach trading the right way. We believe everyone can learn to trade successfully, as long as they're willing to try. With countless hours of experience both trading and teaching, we're here to help you focus your efforts, avoid common mistakes and become self-sufficient.`,
+    `Avium was created by traders who wanted to teach trading the right way. We believe everyone can learn to trade successfully, as long as they're willing to try. With countless hours of experience in both trading and teaching, we're here to help you focus your efforts, avoid common mistakes and become self-sufficient.`,
     `We know trading can be tough because we've been through it ourselves. From chasing the highs and selling the lows to taking revenge trades, we've made our share of errors. It took us years to learn, but now we're here to make it simpler for you.`,
     `That's why at Avium, we focus on helping those who are truly committed to learning. We believe anyone can become a successful trader with dedication and effort. Let us guide you every step of the way to trading success!`,
   ],
