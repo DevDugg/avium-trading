f89b2c32ab8bba7f397ac68996e7cc1ef81ad298
ThePidgeon <97064203+ThePidgeon@users.noreply.github.com>
2024-09-02 18:01:10 +0200

    Update hero.data.ts

--- src/data/hero.data.ts
+++ src/data/hero.data.ts
@@ -1,6 +1,6 @@
 export const heroData = {
   title: "Learn to Trade Like a Pro",
-  subtitle: "Unlock your Trading Potential with Personalized Mentorship and High-Quality Resources",
+  subtitle: "From Beginner to Confident Trader: Personalized Mentorship to Help You Succeed",
   cta: {
     text: "Get started",
     link: "#pricing",
