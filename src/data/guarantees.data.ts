5759d21162b87a40f3292876d9b94a1eca5a1e86
ThePidgeon <97064203+ThePidgeon@users.noreply.github.com>
2024-09-02 17:53:24 +0200

    Update guarantees.data.ts

--- src/data/guarantees.data.ts
+++ src/data/guarantees.data.ts
@@ -1,21 +1,21 @@
 export const guaranteesData = {
   // The description is on the left side of the guarantees section
-  description: "In just 60 days, we`ll teach you everything you need to trade on your own.",
+  description: "In just 60 days, we'll teach you everything you need to trade on your own.",
   guarantees: [
     {
-      number: 20,
+      number: 15,
       name: "Students Taught",
-      description: "More than 20 students have gone through our various programs.",
+      description: "We've turned over 15+ people into self-sufficient traders.",
     },
     {
-      number: 20,
+      number: 15,
       name: "Recommendations",
-      description: "Out of the 20 students we've taught, all would recommend us to their friends.",
+      description: "All our students would recommend our programs to their friends.",
     },
     {
       number: "10/10",
       name: "Rating",
-      description: "When asking our students we got an average rating of 10/10",
+      description: "Our students gave us an average rating of 10/10",
     },
   ],
 };
