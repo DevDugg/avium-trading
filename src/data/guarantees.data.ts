6ed9aadeff1cfd4a91b012ee88766c571b0d3fde
ThePidgeon <97064203+ThePidgeon@users.noreply.github.com>
2024-10-06 10:44:37 +0200

    Update guarantees.data.ts

--- src/data/guarantees.data.ts
+++ src/data/guarantees.data.ts
@@ -3,19 +3,19 @@ export const guaranteesData = {
   description: "In just 60 days, we'll teach you everything you need to know to trade for yourself.",
   guarantees: [
     {
-      number: 15,
+      number: 20,
       name: "Students Taught",
-      description: "We've turned over 15 people into self-sufficient traders.",
+      description: "We've turned over 20 people into self-sufficient traders.",
     },
     {
-      number: 15,
+      number: 20,
       name: "Recommendations",
       description: "100% of our students would recommend us to their friends.",
     },
     {
-      number: "10/10",
+      number: "9.84/10",
       name: "Rating",
-      description: "Our students gave us an average rating of 10/10",
+      description: "Our students gave us an average rating of 9.84/10",
     },
   ],
 };
