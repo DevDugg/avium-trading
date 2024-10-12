bc40337f5277cfcc72c898fc9ac6008dc0b7c168
ThePidgeon <97064203+ThePidgeon@users.noreply.github.com>
2024-10-12 22:26:03 +0200

    Update pricing.data.ts

--- src/data/pricing.data.ts
+++ src/data/pricing.data.ts
@@ -19,10 +19,10 @@ export const pricingData = {
       },
     },*/
     {
-      title: "1-on-1 Advanced",
-      subtitle: "Our Advanced programs are flexible 1-on-1 programs designed to provide long term support for traders and investors with at least a $100k portfolio. We teach you how to trade and invest in crypto & broader asset markets to achieve your growth goals.",
+      title: "1 - on - 1 Mentorship",
+      subtitle: "Our 1-on-1 programs are flexible programs designed to provide long term support for traders and investors with portfolios greater than $100k. We'll teach you how to trade and invest in crypto & broader asset markets to achieve your growth goals.",
       note: "",
-      features: ["Retainers", "Customized Programs", "$100k Minimum Portfolio Size",],
+      features: ["Retainers", "Customized Programs", "Trading & Investing",],
       cta: {
         text: "Book a Call",
         link: "#contact",
