c861667f0125e5c230931c7ba76a2d30f0ee3f01
ThePidgeon <97064203+ThePidgeon@users.noreply.github.com>
2024-10-12 22:50:41 +0200

    Update pricing-card.tsx

--- src/components/pricing/pricing-card.tsx
+++ src/components/pricing/pricing-card.tsx
@@ -32,6 +32,7 @@ const PricingCard = ({ features, note, subtitle, title, price, cta }: PricingCar
         )}
       </div>*/}
       <div
+        id="mentorship"
         className="pricing-card px-6 py-10 rounded-3xl bg-LIGHTBLACK grid grid-cols-TWO gap-6 max-lg:flex max-lg:flex-col max-lg:gap-8 max-[420px]:py-6">
         <div className="flex flex-col gap-10 justify-between max-[420px]:gap-6">
           <div className="flex flex-col gap-6 max-[420px]:gap-4">
