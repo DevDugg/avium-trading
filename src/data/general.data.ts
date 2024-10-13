9cb391608a9bad5437665fe262ae94f49a7d7c28
ThePidgeon <97064203+ThePidgeon@users.noreply.github.com>
2024-10-13 23:33:46 +0200

    Update general.data.ts

--- src/data/general.data.ts
+++ src/data/general.data.ts
@@ -3,7 +3,7 @@ export const generalData = {
   appDescription: "Unlock your Trading Potential with Personalized Mentorship and High-Quality Resources",
   domainName: "avium.trade",
   //   This option will enable/disable scroll animations ('true' to enable, 'false' to disable)
-  useScrollAnimations: false,
+  useScrollAnimations: true,
   //   This option will enable/disable the entry animation ('true' to enable, 'false' to disable)
   useEntryAnimation: true,
 };
